50738db7cac1f96d2457647ee241a7dde1cedcbd
göksel <gurlukgoksel@gmail.com>
2024-03-30 20:14:16 +0300

    Initial commit

---
+++ src/sqlConnection.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class sqlConnection {
+    public static final String DB_URL = "jdbc:mysql://localhost/company";
+
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "mysql";
+
+    public static void main(String[] args) {
+        Connection conn=null;
+        Statement st=null;
+        String sql ="SELECT * FROM employees";
+
+        try{
+            conn= DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            st=conn.createStatement();
+            ResultSet resultSet = st.executeQuery(sql);
+            while(resultSet.next()){
+                System.out.println("İsim: "+resultSet.getString("name"));
+                System.out.println("Pozisyon: "+resultSet.getString("position"));
+                System.out.println("Maaş: "+resultSet.getDouble("salary"));
+                System.out.println("#########################");
+
+            }
+            st.close();
+            conn.close();
+
+        }catch(SQLException ex){
+            System.out.println("SQLException: "+ex.getMessage());
+        }
+
+    }
+
+}
